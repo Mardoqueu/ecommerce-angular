5e83a650c1d527389c4f6402c5f6040c6c50851d
Mardoqueu Sousa <mdq767@gmail.com>
2023-01-17 17:04:06 -0300

    seller-auth component

---
+++ src/app/seller-auth/seller-auth.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seller-auth',
+  templateUrl: './seller-auth.component.html',
+  styleUrls: ['./seller-auth.component.css']
+})
+export class SellerAuthComponent {
+
+}
