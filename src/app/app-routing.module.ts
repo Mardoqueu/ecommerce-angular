578f2ec62148b0c2f5b6b0420a2ac426a4096500
Mardoqueu Sousa <mdq767@gmail.com>
2023-01-17 17:05:19 -0300

    adding routes for seller-auth and home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SellerAuthComponent } from './seller-auth/seller-auth.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component:HomeComponent,
+    path: ''
+  },{
+    component: SellerAuthComponent,
+    path:'seller-auth'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
