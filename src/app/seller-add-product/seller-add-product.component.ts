80137900c15f6a188d3440aabbaec6ed0b7594f1
Mardoqueu Sousa <mdq767@gmail.com>
2023-01-20 13:55:14 -0300

    angular properties and inputs values

--- src/app/seller-add-product/seller-add-product.component.ts
+++ src/app/seller-add-product/seller-add-product.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-seller-add-product',
   templateUrl: './seller-add-product.component.html',
   styleUrls: ['./seller-add-product.component.css']
 })
-export class SellerAddProductComponent {
+export class SellerAddProductComponent implements OnInit {
+  constructor(){}
+
+  ngOnInit(): void {      
+  }
+
+  submit(data:object){
+    console.warn(data);
+  }
 
 }
