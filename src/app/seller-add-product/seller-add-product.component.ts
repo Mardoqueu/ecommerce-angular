66552f833976e68e7c4fe672e4f430d03c1f4080
Mardoqueu Sousa <mdq767@gmail.com>
2023-01-20 14:53:10 -0300

    service function for adding product

--- src/app/seller-add-product/seller-add-product.component.ts
+++ src/app/seller-add-product/seller-add-product.component.ts
@@ -1,18 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { product } from '../data-type';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-seller-add-product',
   templateUrl: './seller-add-product.component.html',
-  styleUrls: ['./seller-add-product.component.css']
+  styleUrls: ['./seller-add-product.component.css'],
 })
 export class SellerAddProductComponent implements OnInit {
-  constructor(){}
+  addProductMessage: string | undefined;
+  constructor(private product: ProductService) {}
 
-  ngOnInit(): void {      
+  ngOnInit(): void {}
+
+  submit(data: product) {
+    this.product.addProduct(data).subscribe((result) => {
+      console.warn(result);
+      if (result) {
+        this.addProductMessage = 'Producut is added sucessfully!';
+      }
+    });
+    setTimeout(() => {
+      this.addProductMessage = undefined;
+    },3000)
   }
-
-  submit(data:object){
-    console.warn(data);
-  }
-
 }
