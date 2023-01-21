042ce6c8d0e84c0797243c26e9a7a65a2653cb8f
Mardoqueu Sousa <mdq767@gmail.com>
2023-01-20 21:52:03 -0300

    carousel images basic

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+	images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+
+  constructor(){}
+
+  ngOnInit(): void {}
 
 }
