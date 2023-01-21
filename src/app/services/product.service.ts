861057035bee72b0a277ac709a6ff930a4f832c7
Mardoqueu Sousa <mdq767@gmail.com>
2023-01-20 22:22:22 -0300

    service function for call selected products api

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -26,4 +26,7 @@ export class ProductService {
   updateProduct(product:product){
     return this.http.put<product>(`http://localhost:3000/products/${product.id}`, product);
   }
+  popularProducts(){
+    return this.http.get<product[]>(`http://localhost:3000/products?_limit=4`);
+  }
 }
