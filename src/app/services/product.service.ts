fd6988f6455b0a09bf0ae9fa38876e8f588ebdac
Mardoqueu Sousa <mdq767@gmail.com>
2023-01-22 13:55:12 -0300

    get data from api for currentcart

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -80,5 +80,11 @@ export class ProductService {
   removeToCart(cartId: number) {
     return this.http.delete('http://localhost:3000/cart/' + cartId);
   }
+
+  currentCart() {
+    let userStore = localStorage.getItem('user');
+    let userData = userStore && JSON.parse(userStore);
+    return this.http.get<cart[]>('http://localhost:3000/cart?userId=' + userData.id);
+  }
    
 }
