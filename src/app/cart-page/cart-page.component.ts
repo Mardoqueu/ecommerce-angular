87525f1a997814ee5b7455242ceb3fae1cc3e52a
Mardoqueu Sousa <mdq767@gmail.com>
2023-01-22 15:18:17 -0300

    bug fix

--- src/app/cart-page/cart-page.component.ts
+++ src/app/cart-page/cart-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit  } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { cart, priceSummary } from '../data-type';
 import { ProductService } from '../services/product.service';
@@ -9,39 +9,56 @@ import { ProductService } from '../services/product.service';
   styleUrls: ['./cart-page.component.css']
 })
 export class CartPageComponent implements OnInit {
-  cartData:cart[]|undefined;
-  priceSummary:priceSummary={
-    price:0,
-    discount:0,
-    tax:0,
-    delivery:0,
-    total:0
+  cartData: cart[] | undefined;
+  priceSummary: priceSummary = {
+    price: 0,
+    discount: 0,
+    tax: 0,
+    delivery: 0,
+    total: 0
   }
-    constructor(private product:ProductService, private router:Router) { }
-  
-    ngOnInit(): void {
-      this.product.currentCart().subscribe((result)=>{
-        this.cartData=result;
-        console.warn(this.cartData);
-        let price=0;
-        result.forEach((item)=>{
-          if(item.quantity){
-            price=price+ (+item.price* +item.quantity)
-          }
-        })
-        this.priceSummary.price=price;
-        this.priceSummary.discount=price/10;
-        this.priceSummary.tax=price/10;
-        this.priceSummary.delivery=100;
-        this.priceSummary.total=price+(price/10)+100-(price/10);
-  
-        console.warn(this.priceSummary);
-        
+  constructor(private product: ProductService, private router: Router) { }
+
+  ngOnInit(): void {
+   this.loadDetails()
+
+  }
+
+  removeToCart(cartId:number|undefined){
+    cartId && this.cartData && this.product.removeToCart(cartId)
+    .subscribe((result)=>{
+      this.loadDetails();
+    })
+  }
+
+  loadDetails(){
+    this.product.currentCart().subscribe((result) => {
+      this.cartData = result;
+      console.warn(this.cartData);
+      let price = 0;
+      result.forEach((item) => {
+        if (item.quantity) {
+          price = price + (+item.price * +item.quantity)
+        }
       })
-  
+      this.priceSummary.price = price;
+      this.priceSummary.discount = price / 10;
+      this.priceSummary.tax = price / 10;
+      this.priceSummary.delivery = 100;
+      this.priceSummary.total = price + (price / 10) + 100 - (price / 10);
+
+    if(!this.cartData.length){
+      this.router.navigate(['/'])
     }
-    
-    checkout(){
-      this.router.navigate(['/checkout']);
-    }
+
+    })
   }
+
+
+
+
+  checkout() {
+    this.router.navigate(['/checkout'])
+  }
+
+}
