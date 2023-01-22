aac9bd657b16f8f2a5c214d90ddd61431139e2aa
Mardoqueu Sousa <mdq767@gmail.com>
2023-01-21 22:02:58 -0300

    service function to add cart ni localStorage

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,12 +1,12 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { product } from '../data-type';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-
+  cartData= new EventEmitter<product[]| []>()
   constructor(private http:HttpClient) { }
   addProduct(data:product){
    return this.http.post('http://localhost:3000/products', data);
@@ -36,5 +36,30 @@ export class ProductService {
 
   searchProduct(query:string){
     return this.http.get<product[]>(`http://localhost:3000/products?_q=${query}`);
-  }
+  } 
+
+  localAddToCart(data:product){
+    let cartData =[];
+    let localCart= localStorage.getItem('localCart');
+    if(!localCart){
+      localStorage.setItem('localCart',JSON.stringify([data]));
+    }else{
+     cartData = JSON.parse(localCart);
+     cartData.push(data);
+     localStorage.setItem('localCart',JSON.stringify(cartData));
+     this.cartData.emit(cartData);
+    }
+   }
+
+   removeItemFromCart(productId:number){
+    let cartData= localStorage.getItem('localCart');
+    if(cartData){
+      let items:product[] =JSON.parse(cartData);
+      items= items.filter((item:product)=>productId!==item.id)
+      localStorage.setItem('localCart',JSON.stringify(items));
+      this.cartData.emit(items);
+    }
+   }
+   
+   
 }
