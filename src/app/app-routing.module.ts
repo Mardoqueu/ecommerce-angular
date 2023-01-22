3f7c91edb0f3b3dd017597b096fa59c0c23828a1
Mardoqueu Sousa <mdq767@gmail.com>
2023-01-22 14:00:38 -0300

    creating routing for checkout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth.guard';
 import { CartPageComponent } from './cart-page/cart-page.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 import { HomeComponent } from './home/home.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { SearchComponent } from './search/search.component';
@@ -55,6 +56,10 @@ const routes: Routes = [
     component: CartPageComponent,
     path:'cart-page'
 
+  },
+  {
+    component: CheckoutComponent,
+    path: 'checkout'
   }
 ];
 
