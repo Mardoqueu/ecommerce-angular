c6aacd87e4d2ff378f59c75f2ce38d8393497cad
Mardoqueu Sousa <mdq767@gmail.com>
2023-01-22 14:25:56 -0300

    ordernow method

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { order } from '../data-type';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css']
 })
-export class CheckoutComponent {
+export class CheckoutComponent implements OnInit {
+
+  totalPrice:number|undefined;
+  constructor(private product:ProductService) { }
+
+  ngOnInit(): void {
+    this.product.currentCart().subscribe((result)=>{
+
+      let price=0;
+      result.forEach((item)=>{
+        if(item.quantity){
+          price=price+ (+item.price* +item.quantity)
+        }
+      })
+      this.totalPrice=price+(price/10)+100-(price/10);
+
+      console.warn(this.totalPrice);
+
+    })
+
+  }
+  orderNow(data:{email:string,address:string,contact:string}){
+    let user = localStorage.getItem('user');
+    let userId= user && JSON.parse(user).id;
+    if(this.totalPrice){
+     let orderData:order={
+        ...data,
+       totalPrice:this.totalPrice,
+       userId
+      }
+
+      this.product.orderNow(orderData).subscribe((result)=>{
+        if(result){
+          alert('Order placed')
+        }
+
+      })
+    }
+
+  }
 
 }
